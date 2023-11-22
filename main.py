3b426dd95c339fe422071521c72032ec63dca0a6
MariaTsiupka <tsiupka2305@gmail.com>
2023-11-22 15:56:53 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+#dictionaries
+hash(1234)
+list_1 = [1,2,3]
+hash(list_1)
+tuple_1 = ("n", "y", 1,2)
+hash(tuple_1)
+dict_1 = {}
+dict_2 = dict()
+dict_3 = ("key_1": 123, "'key_2": "python", 123: "new_value", 3.14: "pi")
+if "key_1 in dict_3"
+    print(dict_3['key_4'])
+else:
+    print()
+    
+dict_3.get('key_4', key does not exist")
+dict_3['key_1'] = "Python"
+print(dict_3)
+
+d =(
+    "point_x":1,
+    "point_y":2,
+    "figure": {
+        "point_x: 2,"
+        "point_y:34,}
+)
+for key in dict_3:
+    print(key)
+for value in dict_3.values():
+    print(f"Value = value")
+len(dict_3)
+for data in dict_3.items():
+    print(data)
+    for key,value in dict_3.item():
+        print(f "dict[(key)]= (value)")
+
+
